057d4cfb9fe6dfc1ae67207319e33c07da8ef2e8
Nguyen <giaanguyen221197@gmail.com>
2022-04-04 11:50:39 +0700

    update

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
-let articles = []
-
 app.use(cors(corsOptions));
 const url = 'https://www.theguardian.com/uk';
 
@@ -21,6 +19,7 @@ const webScarping = async (res) => {
     ignoreDefaultArgs: ['--disable-extensions'],
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
+  const articles = [];
 
   const page = await browser.newPage();
 
@@ -29,25 +28,28 @@ const webScarping = async (res) => {
   const html = await page.content();
   const $ = cheerio.load(html);
 
-  $('.fc-item__container', html).each(async function () {
-    const title = $(this).find('.fc-item__title').text();
+  $('.fc-item__container', html).each(function () {
+    const title = $(this)
+      .find('.fc-item__title')
+      .text()
+      .replace('\n', '')
+      .trim();
     const url = $(this).find('a').attr('href');
     const thumbnail = $(this)
       .find('.fc-item__image-container > picture > img')
       .attr('src');
 
-    const description = $(this).find('.fc-item__standfirst').text().replace("\n", '').trim();
-    const article = { title, url, thumbnail, description };
-    if (article.thumbnail && article.url && article.thumbnail && article.description) {
-      articles.push(article)
-    };
+    const description = $(this)
+      .find('.fc-item__standfirst')
+      .text()
+      .replace('\n', '')
+      .trim();
+    if (thumbnail && description) {
+      articles.push({ title, url, thumbnail, description });
+    }
   });
 
-  articles = Array.from(new Set(articles.map(a => a.title)))
-  .map(title => {
-    return articles.find(a => a.title === title)
-  })
-
+  await browser.close();
   res.send(articles);
 };
 
@@ -57,64 +59,44 @@ const scarpArticle = async (urlLink, res) => {
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
 
-  if (articles.length > 0) {
-    const article = articles.find((article) => article.url.includes(urlLink));
-    const newPage = await browser.newPage();
-    await newPage.goto(article.url);
+  const page = await browser.newPage();
+  await page.goto(url);
 
-    const html = await newPage.content();
-    const $$ = cheerio.load(html);
-    const sections = [];
-    const image = $$('picture > img').attr('src');
+  const html = await page.content();
+  const $ = cheerio.load(html);
 
-    $$('.article-body-commercial-selector > p', html).each(async function(a) {
-      sections.push($$(this).text());
-    });
+  $('.fc-item__container', html).each(async function () {
+    const title = $(this).find('.fc-item__title').text();
+    const link = $(this).find('a').attr('href');
+    const thumbnail = $(this)
+      .find('.fc-item__image-container > picture > img')
+      .attr('src');
 
-    article['image'] = image;
-    article['contentSections'] = sections;
-    res.send(article);
-    newPage.close();
-  } else {
-    const page = await browser.newPage();
-    const articlesArray = [];
-    await page.goto(url);
+    if (link.includes(urlLink)) {
+      await page.goto(link);
+      const sections = [];
 
-    const html = await page.content();
-    const $ = cheerio.load(html);
+      const newHtml = await page.content();
 
-    $('.fc-item__container', html).each(async function () {
-      const title = $(this).find('.fc-item__title').text();
-      const link = $(this).find('a').attr('href');
-      const thumbnail = $(this)
-        .find('.fc-item__image-container > picture > img')
-        .attr('src');
+      const $$ = cheerio.load(newHtml);
+      $$('.article-body-commercial-selector > p', newHtml).each(function (a) {
+        sections.push($$(this).text());
+      });
 
-      const article = { title, url: link, thumbnail };
+      const image = $$('picture > img').attr('src');
 
-      setTimeout(async () => {
-        if (article.url.includes(urlLink)) {
-          const newPage = await browser.newPage();
-          await newPage.goto(article.url);
-          const sections = [];
-          console.log(article.url);
-          const html = await newPage.content();
-          const $$ = cheerio.load(html);
-          $$('.article-body-commercial-selector > p', html).each(async function(a) {
-            sections.push($$(this).text());
-          });
+      const article = {
+        title,
+        url: link,
+        thumbnail,
+        contentSections: sections,
+        image,
+      };
 
-          const image = $$('picture > img').attr('src');
-          article['contentSections'] = sections;
-          article['image'] = image;
-          articlesArray.push(article);
-
-          newPage.close();
-          res.send(article);
-        }
-      }, 1000);
-    });
-  }
+      await page.close();
+      res.send(article);
+    }
+  });
 };
 
 app.get('/api/articles', (req, res) => {
@@ -125,6 +107,6 @@ app.get('/api/article/:url', (req, res) => {
   scarpArticle(req.params.url, res);
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
   console.log(`Web Scarper is running on PORT ${port}`);
 });
